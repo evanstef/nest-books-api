cd16529ab530d320ce2585bc3085d471e8281c8e
EvanSC <evangundala@gmail.com>
2025-01-08 11:20:59 +0700

    main ts

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,28 @@ import { ValidationPipe } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 
-async function bootstrap() {
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+//   const prisma = new PrismaClient();
+
+//   // menggunakan logger dari winston
+//   app.useLogger(logger);
+
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+export default async function handler(req: any, res: any): Promise<void> {
+  // Buat instance aplikasi NestJS
   const app = await NestFactory.create(AppModule);
 
-  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
-  const prisma = new PrismaClient();
+  // Inisialisasi aplikasi (tanpa global prefix)
+  await app.init();
 
-  // menggunakan logger dari winston
-  app.useLogger(logger);
-
-
-  await app.listen(process.env.PORT ?? 3000);
+  // Tangkap request dan response untuk serverless environment di Vercel
+  const server = app.getHttpAdapter().getInstance();
+  server(req, res);
 }
-bootstrap();
